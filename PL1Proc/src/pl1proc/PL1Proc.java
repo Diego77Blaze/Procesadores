1e3f7d7644833ab759b26681aca8d44870df8de8
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2019-09-21 18:50:56 +0200

    Update PL1Proc.java

--- PL1Proc/src/pl1proc/PL1Proc.java
+++ PL1Proc/src/pl1proc/PL1Proc.java
@@ -5,6 +5,10 @@
  */
 package pl1proc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Taiyou
@@ -14,8 +18,13 @@ public class PL1Proc {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numEstados = 0;
+        
+        System.out.println("Cuantos estados tiene la matriz? (Contando el inicial q0)");
+        numEstados = Integer.parseInt(br.readLine());
     }
     
 }
