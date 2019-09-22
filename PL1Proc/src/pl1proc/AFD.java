b787a7b63b65fd1239d766973cee362a636c77b6
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2019-09-22 23:33:02 +0200

    Update AFD.java

--- PL1Proc/src/pl1proc/AFD.java
+++ PL1Proc/src/pl1proc/AFD.java
@@ -38,9 +38,30 @@ public class AFD {
     }
     
     public void establecerQf(List<Integer> ests){
-        estadosFinales.add(5);
+        estadosFinales.add(5); //ejemplo, hacen falta más
     }
     
+    public void inicializacionMatriz(){
+        for(int i = 0; i<estados.size();i++){
+            matriz.put(estados.get(i), new HashMap<Character, Integer>());
+        }
+    }
+    
+    public void cargarMatriz(){
+        matriz.get(0).put('a', 3); //ejemplo, hacen falta más
+    }
+    
+    public Integer getSiguienteEstado(Integer estado,Character caracter){
+        return matriz.get(estado).get(caracter);
+    }
+    
+    public boolean isFinal(Integer estado){
+        return estadosFinales.contains(estado);
+    }
+    
+    public Integer getEstadoInicial(){
+        return estadoInicial;
+    }
     
     
     
