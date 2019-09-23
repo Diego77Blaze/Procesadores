3533cd2905e2cd68a626a949e8e2812535b74619
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2019-09-23 10:21:14 +0200

    Update ME.java

--- PL1Proc/src/pl1proc/ME.java
+++ PL1Proc/src/pl1proc/ME.java
@@ -25,4 +25,24 @@ public class ME {
         estadoActual = automata.getEstadoInicial();
     }
     
+    public void acepta(Character caracter) throws Exception{
+        Integer estadotmp = automata.getSiguienteEstado(estadoActual, caracter);
+        if (estadotmp!=null) estadoActual = estadotmp;
+        else throw new Exception("Algo malo ha pasado.");
+    }
+    
+    public boolean isFinal(){
+        return automata.isFinal(estadoActual);
+    }
+    
+    public boolean compruebaCadena(String s){
+        Inicializar();
+        for (int i=0;i<s.length();i++){
+            try{
+                this.acepta(s.charAt(i));
+                if (this.isFinal()) return true; //HACERLO SÓLO PARA EL FINAL DE TODOS
+            } catch (Exception e){};
+        }
+    }
+    
 }
