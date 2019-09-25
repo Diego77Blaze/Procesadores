9bd6fa18226e9975c2de7c0f6ce2aa9296fb45f3
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2019-09-25 13:37:05 +0200

    Update Main.java

--- PL1Proc/src/pl1proc/Main.java
+++ PL1Proc/src/pl1proc/Main.java
@@ -17,16 +17,16 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("¿Cuantas cadenas desea comprobar?");
-        Integer contadorCadenas = Integer.parseInt(br.readLine());
         ME maquinaEstados = new ME();
         
         
-        boolean resultado = maquinaEstados.compruebaCadena("abcdedede");
-        if (resultado) System.out.println("La cadena es valida para esta expresion regular");
+        boolean resultado1 = maquinaEstados.compruebaCadena("abcdedede");
+        if (resultado1) System.out.println("La cadena es valida para esta expresion regular");
         else System.out.println("La cadena no es valida para esta expresion regular");
         
     }
