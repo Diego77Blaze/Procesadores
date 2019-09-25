a6dbafaed8b35777c6f0692efbf2f1764154187f
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2019-09-25 17:12:27 +0200

    Update Main.java

--- PL1Proc/src/pl1proc/Main.java
+++ PL1Proc/src/pl1proc/Main.java
@@ -8,6 +8,7 @@ package pl1proc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  *
@@ -22,12 +23,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         ME maquinaEstados = new ME();
-        
-        
-        boolean resultado = maquinaEstados.compruebaCadena("abcdede");
-        if (resultado) System.out.println("La cadena SI es valida para esta expresion regular");
-        else System.out.println("La cadena NO es valida para esta expresion regular");
-        
+        String cadena1 = "abcdede";
+        String cadena2 = "abce";
+        ArrayList<String> cadenas = new ArrayList<>();
+        System.out.println("¿Cuantas cadenas deseas probar?");
+        Integer numeroCadenas = Integer.parseInt(br.readLine());
+        for (int i = 0; i<numeroCadenas;i++){
+            System.out.println("Introduce la cadena numero " + i);
+            cadenas.add(br.readLine());
+        }
+        for (int i = 0; i<numeroCadenas;i++){
+            boolean resultado = maquinaEstados.compruebaCadena(cadenas.get(i));
+            if (resultado) System.out.println("La cadena solicitada"+"["+cadenas.get(i)+"] " + "SI es valida para esta expresion regular");
+            else System.out.println("La cadena"+"["+cadenas.get(i)+"] " + "NO es valida para esta expresion regular");
+        }
+
     }
     
 }
