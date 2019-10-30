0e6307a2af37a23127dae75367f7751de0dc7459
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2019-10-30 23:42:24 +0100

    Update ListenerBasico.java

--- PL2Proc/antlr/gramaticas/ListenerBasico.java
+++ PL2Proc/antlr/gramaticas/ListenerBasico.java
@@ -6,5 +6,23 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import java.util.*;
 
 public class ListenerBasico extends GramaticacompilerParserBaseListener{
-    
+    Fichero fichero = Fichero.getInstancia();
+
+    @Override
+    public void enterProg(GramaticacompilerParser.ProgContext ctx){
+        fichero.introducirDatoActual("prog");
+    }
+
+    @Override public void exitProg(GramaticacompilerParser.ProgContext ctx){
+        fichero.eliminarUltimoElementoAñadido();
+    }
+
+    @Override public void enterInclude(GramaticacompilerParser.IncludeContext ctx){
+        fichero.introducirDatoActual("include");
+    }
+
+    @Override public void exitInclude(GramaticacompilerParser.LlamarfuncionContext ctx){
+        fichero.eliminarUltimoElementoAñadido();
+        //ctx.getParent().getRuleIndex();
+    }
 }
