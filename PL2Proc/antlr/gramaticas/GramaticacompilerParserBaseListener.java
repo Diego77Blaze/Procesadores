5b3a92b3ff9c26f9c7840d1e6cf11d1d89c814fb
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2019-10-30 18:52:07 +0100

    Skere

--- PL2Proc/antlr/gramaticas/GramaticacompilerParserBaseListener.java
+++ PL2Proc/antlr/gramaticas/GramaticacompilerParserBaseListener.java
@@ -1,4 +1,4 @@
-// Generated from GramaticacompilerParser.g4 by ANTLR 4.7.2
+// Generated from .\GramaticacompilerParser.g4 by ANTLR 4.7.2
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
@@ -190,6 +190,18 @@ public class GramaticacompilerParserBaseListener implements GramaticacompilerPar
 	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void exitNombrefuncion(GramaticacompilerParser.NombrefuncionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterNomlib(GramaticacompilerParser.NomlibContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitNomlib(GramaticacompilerParser.NomlibContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
